9a754deda1da4a33c65caa713def952b8fda92f5
ObservedObserver <270001151@qq.com>
2019-05-17 17:34:57 +0800

    typescript config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
+
   output: {
-    filename: 'bundle.js',
-    libraryTarget: 'umd',
-    path: path.resolve(__dirname, 'dist')
+      filename: "bundle.js",
+      libraryTarget: 'commonjs',
+      path: path.resolve(__dirname, 'dist')
   },
-  mode: 'development',
-  target: 'node'
+  target: 'node',
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  },
+
+  module: {
+      rules: [
+          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+          { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+      ],
+  },
+
+  // Other options...
 };
